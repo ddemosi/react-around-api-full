fc2ed70f96d32a6c75ccdf73f51c3430dbe23d30
Danny Demosi <ddemosi@remodal.io>
2020-11-22 11:41:58 -0700

    fixed api endpoints

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -25,7 +25,7 @@
 // const removeCardID = document.querySelector('.form__card-id');
 // //api data
 const apiEndpointsCards = {
-  url: "http://danny-demosi.students.nomoreparties.site",
+  url: "http://api.danny-demosi.students.nomoreparties.site",
     headers: { 
       // authorization: "98dd8ac9-99ea-4ab0-85f9-6d61f9934e14",
      "Content-Type": "application/json"
@@ -33,7 +33,7 @@ const apiEndpointsCards = {
 }
 
 const apiEndpointsRegister = {
-  url:"http://danny-demosi.students.nomoreparties.site",
+  url:"http://api.danny-demosi.students.nomoreparties.site",
   headers:{
     "Content-Type": "application/json",
   },
